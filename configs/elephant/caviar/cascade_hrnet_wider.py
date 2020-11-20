024780acc143c14b95a6a305ff92f00cc0863b91
Morten Erfurt Hansen <201608514@post.au.dk>
2020-11-20 21:04:30 +0100

    Changed name of referenced model

--- configs/elephant/caviar/cascade_hrnet_wider.py
+++ configs/elephant/caviar/cascade_hrnet_wider.py
@@ -281,7 +281,7 @@ log_config = dict(
 total_epochs = 20
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/caviar_cascade_rcnn_hrnetv2p_w32'
+work_dir = './work_dirs/cityperson_cascade_rcnn_hrnetv2p_w32'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
